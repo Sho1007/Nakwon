fb0e9253cf6a0292f552f73df7e4d9379572780d
Sho1007 <sho1007@kookmin.ac.kr>
2023-06-28 12:30:11 +0900

    Create Unreal Project

---
+++ Source/Nakwon/NakwonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NakwonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKWON_API ANakwonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
